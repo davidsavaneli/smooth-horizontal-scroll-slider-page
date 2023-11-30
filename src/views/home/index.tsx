41b8f1f92657649d9b39001f27db32b966b5c613
sava <david_savaneli@yahoo.com>
2023-12-01 00:33:00 +0400

    test

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { SliderContainer, SliderItem } from "smooth-horizontal-scroll-slider";
 
 import styles from "./styles.module.css";
 
 const View = () => {
   return (
     <div className={styles.test}>
-      home
+      <SliderContainer>
+        <SliderItem>1</SliderItem>
+        <SliderItem>2</SliderItem>
+        <SliderItem>3</SliderItem>
+      </SliderContainer>
     </div>
   );
 };
