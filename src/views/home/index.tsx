0d5439f44fcfa9de2b9512525f1ae9e6be7d5934
sava <david_savaneli@yahoo.com>
2023-12-04 18:23:11 +0400

    test version 0.1.2

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { SliderContainer, SliderItem } from 'smooth-horizontal-scroll-slider'
+import { SliderContainer, SliderWrapper, SliderItem } from 'smooth-horizontal-scroll-slider'
 
 const View = () => {
   return (
-    <SliderContainer>
-      <SliderItem>1</SliderItem>
-      <SliderItem>2</SliderItem>
-      <SliderItem>3</SliderItem>
+    <SliderContainer speed={2} progressBar={{ show: true, position: 'bottom' }}>
+      <SliderWrapper direction='ltr' items={{ fullWidth: true, fullHeight: true }}>
+        <SliderItem>1</SliderItem>
+        <SliderItem>2</SliderItem>
+        <SliderItem>3</SliderItem>
+      </SliderWrapper>
     </SliderContainer>
   )
 }
